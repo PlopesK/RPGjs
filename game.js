55165efd27a25544ca22c6e25c2312302bb4fba6
PlopesK <gp081009@gmail.com>
2025-02-25 18:44:22 -0300

    Player Spawn in the map!

--- game.js
+++ game.js
@@ -1,4 +1,3 @@
-const map = document.querySelector("body")
 const canvas = document.querySelector("canvas");
 const cont = canvas.getContext("2d"); //Context
 
@@ -10,6 +9,22 @@ cont.fillRect(0, 0, canvas.width, canvas.height); //X, Y, Width, Height
 
 const image = new Image();
 image.src = './img/PelletTown.png'
+
+const playerImg = new Image();
+playerImg.src = './img/playerDown.png'
+
 image.onload = function(){
-    cont.drawImage(image, -550, -550); //Image, X, Y
+    cont.drawImage(image, -566, -560); //Image, X, Y
+    cont.drawImage(
+        playerImg, 
+        0,
+        0,
+        playerImg.width / 4,
+        playerImg.height,
+        canvas.width / 2 - (playerImg.width / 4) / 2, 
+        canvas.height / 2 - playerImg.height / 2,
+        playerImg.width / 4,
+        playerImg.height,
+    ); //Image, Crop X, Crop Y, Crop Width, Crop Height, 
+      // Image X, Image Y, Image Width, Image Height
 }
