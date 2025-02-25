974fcbddb649a7b537c820f19948af761770fabc
PlopesK <gp081009@gmail.com>
2025-02-25 19:08:07 -0300

    Created class sprite to simplify things

--- game.js
+++ game.js
@@ -7,14 +7,36 @@ canvas.height = window.innerHeight;
 cont.fillStyle = 'White';
 cont.fillRect(0, 0, canvas.width, canvas.height); //X, Y, Width, Height
 
-const image = new Image();
-image.src = './img/PelletTown.png'
+const backImg = new Image();
+backImg.src = './img/PelletTown.png'
 
 const playerImg = new Image();
 playerImg.src = './img/playerDown.png'
 
-image.onload = function(){
-    cont.drawImage(image, -566, -560); //Image, X, Y
+class Sprite {
+    constructor({position, image, velocity}) {
+        this.position = position
+        this.image = image
+        this.velocity = velocity
+    }
+
+    draw () {
+        cont.drawImage(this.image, -566, -560)
+    }
+}
+
+const background = new Sprite ({
+    position: {
+        x: -566,
+        y: -560
+    },
+    image: backImg,
+})
+
+function animate() {
+    console.log("loaded");
+    window.requestAnimationFrame(animate);
+    background.draw() //Image, X, Y
     cont.drawImage(
         playerImg, 
         0,
@@ -28,3 +50,20 @@ image.onload = function(){
     ); //Image, Crop X, Crop Y, Crop Width, Crop Height, 
       // Image X, Image Y, Image Width, Image Height
 }
+animate();
+
+window.addEventListener("keydown", (e) => {
+    switch (e.key) {
+        case 'ArrowUp' || 'W':
+            break;
+
+        case 'ArrowLeft' || 'A':
+            break;
+
+        case 'ArrowDown' || 'S':
+            break;
+
+        case 'ArrowRight' || 'D':
+            break;
+    }
+})
